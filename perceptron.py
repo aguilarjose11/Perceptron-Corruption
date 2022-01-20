932244081012453fe2df228b644732acb1b6bdd3
JoseE. Aguilar Escamilla <aguilar.jose11@outlook.com>
2022-01-20 07:33:21 -0600

    Add extra parameters.

--- perceptron.py
+++ perceptron.py
@@ -8,20 +8,42 @@ class PocketPerceptron:
     """
 
     def __init__(self, 
-    input: int    =10,
-    gamma: float  =1,
-    max_iter: int =1000):
-        """"""
+    input: int      =10,
+    eta: float      =1,
+    max_iter: int   =1000,
+    rand_seed: int  =37):
+        """Create pocket-trained perceptron
+        
+        Following Gallant's theory, initialize a perceptron
+        that shall be trained with the pocket algorithm. 
+        Unlike Gallant's, this algorithm picks random samp-
+        les in each iteration without repetition.
+
+        Parameters:
+        -----------
+        input
+        - Number of inputs for perceptron 
+        
+        eta
+        - Learning rate for algorithm
+        
+        max_iter
+        - Maximum number of iterations of pocket algorithm
+
+        rand_seed
+        - Random seed for random iterator.
+        """
         self.pi         = np.zeros((input, 1))
         self.W          = np.zeros((input, 1))
         self.run_pi     = 0
         self.run_W      = 0
         self.num_ok_pi  = 0
         self.num_ok_W   = 0
-        self.gamma      = gamma
+        self.eta        = eta
         self.max_iter   = max_iter
+        self.rand_seed  = rand_seed
     
-    def solve(self, X) -> bool:
+    def solve(self, X) -> int:
         """Solve using pocket hypothesis
         
         Using the pocketed weights, the perceptron predicts
