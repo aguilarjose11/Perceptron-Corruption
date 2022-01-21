65d63140b9b6f3cbf3f84e3976fae36672a5a7e1
JoseE. Aguilar Escamilla <aguilar.jose11@outlook.com>
2022-01-21 09:33:33 -0600

    Add testing for training and learning

--- tests/test_pocket_perceptron.py
+++ tests/test_pocket_perceptron.py
@@ -1,6 +1,8 @@
 from Perceptron.perceptron import PocketPerceptron
 from tests.conftest import xor_perceptron
 
+import numpy as np
+
 class TestPocketXor:
     def test_xor_unin_perceptron(self, xor_perceptron):
         """Test on uninitialized model"""
@@ -30,3 +32,31 @@ class TestPocketXor:
             assert algo.solve(X) == y
 
         del algo
+
+class TestPocketTraining:
+    def test_learn(self, learn_xor_pocket):
+        algo = PocketPerceptron(
+            input       =3,
+            eta         =1,
+            max_iter    =1000,
+            rand_seed   =37
+        )
+
+        algo.learn(learn_xor_pocket["X"])
+
+        assert algo.pi == learn_xor_pocket["pi"]
+        assert algo.W == learn_xor_pocket["W"]
+        assert algo.run_pi == learn_xor_pocket["run_pi"]
+        assert algo.run_W == learn_xor_pocket["run_W"]
+        
+    def test_train(self, xor_perceptron):
+        algo = PocketPerceptron(
+            input       =3,
+            eta         =1,
+            max_iter    =1000,
+            rand_seed   =37
+        )
+
+        algo.train(xor_perceptron["X"], xor_perceptron["y"])
+
+        assert algo.W != np.zeros((3, 1))
