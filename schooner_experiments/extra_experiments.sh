43bd730701d6bd9c1d9d4fdd52ea9c7b94168750
Jose Efraim Aguilar Escamilla <aguilar.jose11@outlook.com>
2023-02-05 17:54:38 -0600

    Add multithreading limits for Numpy

--- schooner_experiments/extra_experiments.sh
+++ schooner_experiments/extra_experiments.sh
@@ -7,7 +7,7 @@
 #SBATCH --ntasks=1                                                              
 #SBATCH --mem=2G
 #SBATCH --cpus-per-task=4
-#SBATCH --time=03:59:58
+#SBATCH --time=2:59:58
 #SBATCH --array=0-99
 
 echo start time
@@ -15,29 +15,33 @@ date
 
 source activate tda-gpu
 
+export MKL_NUM_THREADS=4
+export NUMEXPR_NUM_THREADS=4
+export OMP_NUM_THREADS=4
+
 # Other experiments are SPECT, spambase, and bankrupcy
-EXPERIMENT="bankrupcy"
+EXPERIMENT="SPECT"
 
 # For SLURM only. It will help in parallel execution. See --array param.
 # Remember that --index is simply an output file index value.
-#EXP_NUMBER=$SLURM_ARRAY_TASK_ID
-#INDEXLABEL=$EXP_NUMBER
+EXP_NUMBER=$SLURM_ARRAY_TASK_ID
+INDEXLABEL=$EXP_NUMBER
 
 # For local execution only. Comment out if in schooner.
-INDEXLABEL=debugging
+#INDEXLABEL=debugging
 
 # Experiment set up
-BUCKETS=100                                                                      
+BUCKETS=100
 NRUNS=1 # If running parallely, [NRUNS * --array] total runs
 
 # Model parameters
 ETA=0.1
-MAXITER=2000             
-PATIENCE=40 
+MAXITER=6000
+PATIENCE=40
 RESROOT=results # DO NOT forget to create the directory!
 TESTFRACT=0.2
 
 python corruption_experiment.py --experiment $EXPERIMENT --bias -t $TESTFRACT -b $BUCKETS -r $NRUNS --eta $ETA --max_iter $MAXITER --w_init -1 1 -v --result_root $RESROOT --index $INDEXLABEL --patience $PATIENCE
-                                                                                
-echo end time                                                                   
-date   
+
+echo end time
+date
